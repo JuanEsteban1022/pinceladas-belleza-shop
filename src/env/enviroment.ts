79786d34292fbf2c08319d4ea38d6ff9d2577558
Juan Esteban <juanesteban1022@gmail.com>
2026-02-20 10:29:34 -0500

    cambio de ruta backend

--- src/env/enviroment.ts
+++ src/env/enviroment.ts
@@ -1,7 +1,7 @@
 const IP = location.hostname;
 
 export const environment = {
-    production: false,
-    API_URL: `https://pinceladas-belleza-backend-gwi8.onrender.com`
-    // API_URL: `http://localhost:8080`
+  production: false,
+  API_URL: `https://app-095677e5-a7e8-47b6-97fd-8dd5ab19e3c8.cleverapps.io`,
+  // API_URL: `http://localhost:8080`
 };
